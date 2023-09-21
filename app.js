2a8bb39dd3f999a9a299b759f782aad96683b666
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-09-21 12:32:21 +0200

    Test re update

--- app.js
+++ app.js
@@ -39,10 +39,10 @@ app.use((req, res) => {
 });
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet({
-  crossOriginResourcePolicy: false,
-}));
+// app.use(express.urlencoded({ extended: true }))
+// app.use(helmet({
+//   crossOriginResourcePolicy: false,
+// }));
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 const db = require("./models");
